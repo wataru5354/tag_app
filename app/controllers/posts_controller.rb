c981820f15bef86c56789e78e63cb3623724ce61
wataru5354 <setsunan76253@gmail.com>
2022-05-04 10:07:17 +0900

    コメントアウト完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,15 +20,19 @@ class PostsController < ApplicationController
   end
 
   def edit
+    # @postのデータをハッシュ型で取得
     post_attributes = @post.attributes
     @post_form = PostForm.new(post_attributes)
+    # 最初のタグがnilの場合はnilを返し、情報があればそのインスタンスを返す
     @post_form.tag_name = @post.tags&.first&.tag_name
   end
 
   def update
     @post_form = PostForm.new(post_form_params)
+    # 画像を選択し直していない場合は、既存の画像をセットする
     @post_form.image ||= @post.image.blob
     if @post_form.valid? 
+      # updateメソッドに二つの引数を渡す
       @post_form.update(post_form_params ,@post)
       redirect_to root_path
     else
--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -1,7 +1,9 @@
 class PostForm
   include ActiveModel::Model
+  # Formオブジェクトを使用する際はここで全てのバリデーションを設定すること
   attr_accessor(
     :text,:image,
+    # 編集の際はidが必要になるので、ここで編集したいidを取得できるようにする
     :id, :created_at, :datetime, :updated_at, :datetime,
     :tag_name
   ) 
@@ -11,18 +13,24 @@ class PostForm
     validates :image
   end
 
+  # controllerから受け取ったデータをsaveメソッドで処理する
   def save
     post = Post.create(text: text,image: image)
-    tag = Tag.where(tag_name: tag_name).first_or_initialize
+    #first_or_initializeメソッドを使用することにより、該当するレコードがあればそのインスタンスを返し、なければ新しくインスタンスを生成することができる
+    tag = Tag.where(tag_name: tag_name).first_or_initialize 
     tag.save
     PostTagRelation.create(post_id: post.id, tag_id: tag.id)
   end
 
   def update(params, post)
+    #タグの紐付けをなくす（タグの上書きを行うため）
     post.post_tag_relations.destroy_all
+    # post_form_paramsからタグ情報を消し、タグ情報を変数に代入
     tag_name = params.delete(:tag_name)
+    # タグモデルからタグの情報を取得する。情報がなければ新しくインスタンスを生成
     tag = Tag.where(tag_name: tag_name).first_or_initialize if tag_name.present?
     tag.save if tag_name.present?
+    # @postにpost_form_paramのデータを更新する
     post.update(params)
     PostTagRelation.create(post_id: post.id,tag_id: tag.id) if tag_name.present?
   end
